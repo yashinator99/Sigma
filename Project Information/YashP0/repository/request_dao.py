e75688b5f0d784d6793a943f4482117b8b83687e
Modesto Amador <mofesdo.dev@gmail.com>
2022-05-30 14:00:00 +0100

    added pytest for validation service

--- Project Information/YashP0/repository/request_dao.py
+++ Project Information/YashP0/repository/request_dao.py
@@ -45,3 +45,20 @@ def get_request(user_id):
     finally:
         if connection is not None:
             connection.close()
+
+def delete_user_request(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    qry = f"DELETE FROM request_table WHERE user_id = '{user_id}';"
+
+    try:
+        cursor.execute(qry)
+        connection.commit()
+        return
+    except(psycopg2.DatabaseError) as error:
+        print(error)
+        connection.rollback()
+    finally:
+        if connection is not None:
+            connection.close()
