a3552c40715feebf5af69e7075c1de517de41b49
Yash <yashinator99@gmail.com>
2022-05-31 21:46:46 -0400

    Changed README file and changed deletion

--- Project Information/YashP0/service/deletion_service.py
+++ Project Information/YashP0/service/deletion_service.py
@@ -1,5 +1,6 @@
 from repository.login_dao import get_user_id, delete_user
 from repository.user_info_dao import delete_user_info
+from repository.request_dao import delete_user_request
 
 def prep_user_id(registration_input):
     user_id = get_user_id(registration_input)
@@ -8,6 +9,7 @@ def prep_user_id(registration_input):
         return user_id
 
 def delete_user_from_db(user_id):
+    delete_user_request(user_id)
     delete_user_info(user_id)
     delete_user(user_id)
 
