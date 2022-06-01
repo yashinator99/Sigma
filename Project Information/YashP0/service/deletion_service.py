a86e617544eb941576a85840c0fd6426d7f05e4a
Modesto Amador <mofesdo.dev@gmail.com>
2022-05-31 23:27:41 -0700

    Revert "added pytest for validation service"
    This reverts commit e75688b5f0d784d6793a943f4482117b8b83687e.

--- Project Information/YashP0/service/deletion_service.py
+++ Project Information/YashP0/service/deletion_service.py
@@ -1,6 +1,5 @@
 from repository.login_dao import get_user_id, delete_user
 from repository.user_info_dao import delete_user_info
-from repository.request_dao import delete_user_request
 
 def prep_user_id(registration_input):
     user_id = get_user_id(registration_input)
@@ -9,7 +8,6 @@ def prep_user_id(registration_input):
         return user_id
 
 def delete_user_from_db(user_id):
-    delete_user_request(user_id)
     delete_user_info(user_id)
     delete_user(user_id)
 
