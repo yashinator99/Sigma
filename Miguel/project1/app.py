d6fb13e3abff8d141a4eba68aabf5e16960be2ff
Jeremy Gray <jgray@brainstation.io>
2022-06-05 19:20:36 -0400

    added redirect functionality

--- Miguel/project1/app.py
+++ Miguel/project1/app.py
@@ -2,17 +2,31 @@ from flask import Flask, request, render_template, redirect, url_for
 from repository.database import check_user_info
 
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return redirect('http://127.0.0.1:5000/login')
 
-@app.route('/login' , methods=['GET'])
+@app.route('/request')
+def reimbursement():
+    return render_template('request.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/approval')
+def approval():
+    return render_template('approval.html')
+
+@app.route('/login' , methods=['GET', 'POST'])
 def confirm_user():
-    if check_user_info(request.form):
-        return render_template('account.html')
-    return render_template('index.html')
+    if request.method == 'POST':
+        if check_user_info(request.form):
+            return render_template('account.html')
+    return render_template('login.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
